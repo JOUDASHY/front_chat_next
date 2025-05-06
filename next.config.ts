306ff2cc58bbf0709b50de7dd9dde1e0fdf911d4
JOUDASHY <alitsiryeddynilsen@gmail.com>
2025-05-07 00:54:14 +0300

    fix next conf

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+// next.config.js
+// Ce fichier doit être en JS pour être pris en compte par Render
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Générer un site 100% statique (remplace next export)
   output: 'export',
   // (Optionnel) Modifier le dossier de sortie si besoin
   // distDir: 'build',  
 };
 
-export default nextConfig;
+module.exports = nextConfig;
